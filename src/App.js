71095c119dab7a133063b31f17e576bda8674d3f
jacobruleaux <jakeruleaux@gmail.com>
2017-10-26 16:54:17 -0700

    generate loginform comp

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+import LogInForm from './components/LogInForm';
 
 class App extends React.Component {
+  componentWillMount(){
+   const config = {
+   apiKey: "AIzaSyATES7bJQllMKGyDl9hD0aHa7hTA-CCW-g",
+   authDomain: "manager-c6155.firebaseapp.com",
+   databaseURL: "https://manager-c6155.firebaseio.com",
+   projectId: "manager-c6155",
+   storageBucket: "manager-c6155.appspot.com",
+   messagingSenderId: "617798770778"
+ };
+ firebase.initializeApp(config);
+}
+
   render() {
     return (
-      <Provider store={createStore()}>
+      <Provider store={createStore(reducers)}>
         <View>
-          <Text>
-          hello
-          </Text>
+          <LogInForm />
         </View>
       </Provider>
     );
---
+++ src/components/LogInForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Card } from './common';
+
+class LogInForm extends React.Component {
+  render() {
+    return (
+      <Card>
+      </Card>
+    );
+  }
+}
+export default LogInForm;
