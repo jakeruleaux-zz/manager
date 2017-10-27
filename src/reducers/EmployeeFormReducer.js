e9708e0c6711529c4bdd9404d13dfe3690bbe085
jacobruleaux <jakeruleaux@gmail.com>
2017-10-27 15:12:08 -0700

    generate authactions file and refactor action index

--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -2,12 +2,16 @@ import {
   EMPLOYEE_UPDATE
 } from '../actions/types';
 
-const INITIAL_STATE = {};
+const INITIAL_STATE = {
+  name: '',
+  phone: '',
+  shift: ''
+};
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.types) {
     case EMPLOYEE_UPDATE:
-      return { ...state, [action.payload.prop]: action.payload.value }
+      return { ...state, [action.payload.prop]: action.payload.value };
     default:
       return state;
   }
