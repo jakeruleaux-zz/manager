d8fb1fceca64fb324092163d9abceb16b8a02eb4
jacobruleaux <jakeruleaux@gmail.com>
2017-10-27 10:04:04 -0700

    install thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,24 @@
-import { EMAIL_CHANGED } from './types';
+import firebase from 'firebase';
+import {
+  EMAIL_CHANGED,
+  PASSWORD_CHANGED
+ } from './types';
 
 export const emailChanged = (text) => {
   return {
-    type: 'email_changed',
+    type: EMAIL_CHANGED,
     payload: text
   };
+};
+
+export const passWordChanged = (text) => {
+  return {
+    type: PASSWORD_CHANGED,
+    payload: text
+  };
+};
+
+export const LoginUser = ({ email, password }) => {
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(user => console.log(user));
 };
--- src/components/LogInForm.js
+++ src/components/LogInForm.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Card, CardSection, Input, Button } from './common';
-import { emailChanged } from '../actions';
+import { emailChanged, passWordChanged } from '../actions';
 
 class LogInForm extends React.Component {
   onEmailChange(text) {
     this.props.emailChanged(text);
+  }
+
+  onPassWordChange(text) {
+    this.props.passWordChanged(text);
   }
 
   render() {
@@ -16,6 +20,7 @@ class LogInForm extends React.Component {
             label="Email"
             placeholder="email@email.com"
             onChangeText={this.onEmailChange.bind(this)}
+            value={this.props.email}
           />
         </CardSection>
         <CardSection>
@@ -23,6 +28,8 @@ class LogInForm extends React.Component {
             secureTextEntry
             label="Password"
             placeholder="Password"
+            onChangeText={this.onPassWordChange.bind(this)}
+            value={this.props.password}
           />
         </CardSection>
         <CardSection>
@@ -35,4 +42,11 @@ class LogInForm extends React.Component {
   }
 }
 
-export default connect(null, { emailChanged })(LogInForm);
+const mapStateToProps = state => {
+  return {
+    email: state.auth.email,
+    password: state.auth.password
+  };
+};
+
+export default connect(mapStateToProps, { emailChanged, passWordChanged })(LogInForm);
