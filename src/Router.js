ec65755310bf771dd754a5888b00d7e5b5fb3510
jacobruleaux <jakeruleaux@gmail.com>
2017-10-27 11:47:29 -0700

    install router flux and generate router comp

---
+++ src/Router.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Scene, Router } from 'react-native-router-flux';
+import LoginForm from './components/LoginForm';
+
+const RouterComponent = () => {
+
+};
+
+export default RouterComponent;
