4ef71f3472812e0724ff9a439c20fbfb89bbac84
jacobruleaux <jakeruleaux@gmail.com>
2017-10-26 17:01:41 -0700

    build out loginform

--- src/components/LogInForm.js
+++ src/components/LogInForm.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import { Card } from './common';
+import { Card, CardSection, Input, Button } from './common';
 
 class LogInForm extends React.Component {
   render() {
     return (
       <Card>
+        <CardSection>
+          <Input
+            label="Email"
+            placeholder="email@email.com"
+          />
+        </CardSection>
+        <CardSection>
+          <Input
+            secureTextEntry
+            label="Password"
+            placeholder="Password"
+          />
+        </CardSection>
+        <CardSection>
+          <Button>
+            Login
+          </Button>
+        </CardSection>
       </Card>
     );
   }
 }
+
 export default LogInForm;
